7c52664d19e39881aed74cf511da301feeb29e48
김진영 <gam0za@naver.com>
2018-06-14 18:36:30 +0900

    add bootstrap

--- dukma/app.js
+++ dukma/app.js
@@ -24,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/js',express.static(path.join(__dirname,'node_modules','bootstrap','dist','js')));
+app.use('/css', express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'css')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
