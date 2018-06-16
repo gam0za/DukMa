ccf501dfab4f08fb132579ab53fe7ebde5f2151b
김진영 <gam0za@naver.com>
2018-06-17 06:10:28 +0900

    login page routing

--- dukma/routes/index.js
+++ dukma/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// 1. 홈페이지
 router.get('/', function(req, res, next) {
   res.render('home.html');
 });
+
+// 2. 계정 (로그인, 회원가입)
+const account=require('./account/account.js');
+router.use('/account',account);
 
 module.exports = router;
